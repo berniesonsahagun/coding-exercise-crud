f2d4c767fed5ae2d55d582d48e11936a2ee8b647
Bernieson Sahagun <berniesonsahagun@gmail.com>
2025-02-13 15:47:31 +0800

    hotfix: fix search outline border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import UsersTable from "@/components/table/UsersTable";
 export default async function Home() {
   return (
     <section className="max-w-[1200px] mx-auto min-h-screen grid items-center px-10">
-      <div className="max-h-[900px] overflow-y-auto">
+      <div className="max-h-[900px]">
         <UsersTable />
       </div>
     </section>
